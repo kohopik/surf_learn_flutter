c1ec0df400607101f05cdb2d70eea20dd2b100e1
Ilya Shumikhin <ilya.shumikhin@rt.ru>
2023-09-19 22:46:30 +0300

    Реализована домашняя работа с Gesture

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -28,40 +30,67 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  double? _top;
+  double? _left;
+  double _angle = 0;
+  double _height = 50;
+  double _width = 50;
+  Color _color = Colors.black;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  _MyHomePageState() {
+    _top = MediaQuery.of(context).size.height / 2 - _height / 2;
+    _left = MediaQuery.of(context).size.width / 2 - _width / 2;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    final AnimationController _rotationController = AnimationController(
+      duration: const Duration(seconds: 6),
+      vsync: this,
     );
+    final Animation<double> _rotationAnimation = CurvedAnimation(
+      parent: _rotationController,
+      curve: Curves.elasticOut,
+    );
+
+    return Scaffold(
+        body: Stack(
+      children: [
+        Positioned(
+          left: _left,
+          top: _top,
+          width: _width,
+          height: _height,
+          child: RotationTransition(
+            turns: _rotationAnimation,
+            child: ColoredBox(
+              color: _color,
+            ),
+          ),
+        ),
+        Positioned.fill(
+            child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _color = Colors
+                        .primaries[Random().nextInt(Colors.primaries.length)];
+                  });
+                },
+                onLongPress: () {
+                  _rotationController.reset();
+                  _rotationController.forward();
+                },
+                onVerticalDragUpdate: _onDragChanged,
+                onHorizontalDragUpdate: _onDragChanged)),
+      ],
+    ));
+  }
+
+  void _onDragChanged(DragUpdateDetails details) {
+    setState(() {
+      _left = details.globalPosition.dx - _width / 2;
+      _top = details.globalPosition.dy - _height / 2;
+    });
   }
 }
