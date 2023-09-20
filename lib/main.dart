787f7d62ceccfd0ef67b122ede7f8366ae1b41f3
Ilya Shumikhin <ilya.shumikhin@rt.ru>
2023-09-20 22:18:18 +0300

    Реализовал задание с ресурсами

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,9 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(fontFamily: 'Helvetica'),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -29,39 +28,80 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      body: Stack(
+        children: [
+          Align(
+              child: Container(
+                  constraints: const BoxConstraints(maxWidth: 400),
+                  alignment: Alignment.center,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: CircleAvatar(
+                              foregroundImage:
+                                  AssetImage('assets/images/avatar.jpg')),
+                        ),
+                      ),
+                      SizedBox(height: 13),
+                      DescriptionWidget(
+                          "ФИО", "Шумихин Илья", "assets/images/person.svg"),
+                      DescriptionWidget(
+                          "О себе",
+                          "27 лет, в свободное время кодю, в несвободное тоже кодю",
+                          "assets/images/about.svg"),
+                      DescriptionWidget(
+                          "Увлечения",
+                          "Поигрываю на гитарке, увлекаюсь музыкой, придумываю хоум проекты и забиваю по-быстрому на них",
+                          "assets/images/hobby.svg"),
+                      DescriptionWidget(
+                          "Опыт в разработке",
+                          "1 год .NET, 5 лет iOS разработки",
+                          "assets/images/experience.svg"),
+                    ],
+                  )))
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    );
+  }
+}
+
+class DescriptionWidget extends StatelessWidget {
+  final String _title;
+  final String _subtitle;
+  final String _imagePath;
+
+  const DescriptionWidget(this._title, this._subtitle, this._imagePath,
+      {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+          SizedBox(width: 30, height: 30, child: SvgPicture.asset(_imagePath)),
+          const SizedBox(width: 4),
+          Text(
+            _title,
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
+          ),
+        ]),
+        const SizedBox(height: 8),
+        Align(
+            alignment: Alignment.topLeft,
+            child: Text(_subtitle,
+                style: const TextStyle(
+                    fontWeight: FontWeight.normal, fontSize: 14.0))),
+        const SizedBox(height: 13)
+      ],
     );
   }
 }
